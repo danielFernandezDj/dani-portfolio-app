13649e6765c5024a81d349b4c2087d3c8b20ddae
Daniel Fernandez <mdanif9728@gmail.com>
2024-10-17 08:38:28 -0700

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HomeIcon, CogIcon, UserIcon } from '@heroicons/react/outline';
+
+// Component Pages
+import Home from './components/LandingPage';
+
+function Projects() {
+  return <div className="p-4"></div>;
+}
+
+function About() {
+  return <div className="p-4"></div>;
+}
+
+// Navigation Bar
+function NavBar() {
+  return (
+    // backdrop-blur-lm
+    <nav className="bg-blue-600 ">
+      <div className="container mx-auto px-4 py-6 flex justify-around items-center md:space-x-48 lg:space-x-72">
+
+        <div className='flex space-x-8 items-center justify-center'>
+          {/* Left side - Logo */}
+          <Link to="/home" className="text-white text-xl font-bold">
+            MyLogo
+          </Link>
+
+          {/* Center - Navigation Links */}
+          {/* <div className="space-x-4">
+            <Link to="/home" className="text-white hover:text-gray-300">Home</Link>
+            <Link to="/projects" className="text-white hover:text-gray-300">Projects</Link>
+            <Link to="/about" className="text-white hover:text-gray-300">About</Link>
+          </div> */}
+        </div>
+
+        {/* Right side - Icons */}
+        <div className="flex space-x-4">
+          <button className="text-white hover:text-gray-300">
+            <HomeIcon className="h-6 w-6" />
+          </button>
+          <button className="text-white hover:text-gray-300">
+            <CogIcon className="h-6 w-6" />
+          </button>
+          <button className="text-white hover:text-gray-300">
+            <UserIcon className="h-6 w-6" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+// Main App
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        {/* Welcome */}
+        <div className="my-6 mx-4">
+          <h1>Welcome to Home page.</h1>
+        </div>
+
+        {/* Project list, Cards */}
+        <div className="my-6 mx-4">
+
+        </div>
+
+        {/* About me text 'p' */}
+        <div className="my-6 mx-4">
+
+        </div>
+
+        {/* Tech That I know list using a 'p' */}
+        <div className="my-6 mx-4">
+
+        </div>
+
+        {/* Something Cool to impress the heir team (like a line that do something). */}
+        <div className="my-6 mx-4">
+
+        </div>
+
+        {/* Contact me From */}
+
+      </section>
+    </main>
+  )
+}
