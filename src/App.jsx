952df429b83d1c111271f452721b5a1dc237bf0b
Daniel Fernandez <mdanif9728@gmail.com>
2024-10-24 11:10:06 -0700

    preventive saving

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { HomeIcon, CogIcon, UserIcon } from '@heroicons/react/outline';
-// Dark Theme Icon
 import { GiMoonBats } from "react-icons/gi";
-// Light Theme Icon
 import { MdSunny } from "react-icons/md";
-// Nav Menu Icon
 import { TiThMenu } from "react-icons/ti";
-
-// import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-
 
 // Component Pages
 import Home from './components/Home';
@@ -26,19 +18,6 @@ function About() {
 
 // Navigation Bar
 function NavBar() {
-
-  // function handleSelectMenu() {
-  //   return (
-  //     <>
-  //       <select className="appearance-none flex flex-col justify-center items-center ">
-  //         <Link to="/home" className="nav-text-style">Home</Link>
-  //         <Link to="/projects" className="nav-text-style">Projects</Link>
-  //         <Link to="/about" className="nav-text-style">About</Link>
-  //       </select>
-  //     </>
-  //   )
-  // }
-
   return (
     <nav>
       <div className="container mx-auto md:px-4 px-8 py-8 flex justify-between md:px-20 items-center">
@@ -53,59 +32,10 @@ function NavBar() {
             <Link to="/about" className="nav-text-style">About</Link>
           </div>
         </div>
-
         {/* –––––––––––––––––––––––––––– Right side - Icons –––––––––––––––––––––––––––– */}
         <div className="flex space-x-6">
-          <Menu as="div" className="relative inline-block text-left">
-            <div>
-              <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                Options
-                <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
-              </MenuButton>
-            </div>
-
-            <MenuItems
-              transition
-              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-            >
-              <div className="py-1">
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                  >
-                    Account settings
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                  >
-                    Support
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                  >
-                    License
-                  </a>
-                </MenuItem>
-                <form action="#" method="POST">
-                  <MenuItem>
-                    <button
-                      type="submit"
-                      className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                    >
-                      Sign out
-                    </button>
-                  </MenuItem>
-                </form>
-              </div>
-            </MenuItems>
-          </Menu>
+            <button className="text-blue-900 lg:hover:text-yellow-500 simple-hover-animation"><MdSunny className="w-6 h-auto" /></button>
+            <button className="text-blue-900 "> <TiThMenu className="w-6 h-auto" /></button>{/* md:hidden */}
         </div>
       </div>
 
