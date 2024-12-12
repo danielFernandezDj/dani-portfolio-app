04f9baebb2e44aaa5fce529a1e3e7a8917837745
Daniel Fernandez <mdanif9728@gmail.com>
2024-12-12 13:26:07 -0800

    Now TLC Licensing is full developed & responsive

--- src/pages/TLCLicensing.jsx
+++ src/pages/TLCLicensing.jsx
@@ -1,17 +1,25 @@
 import React from "react"
 import CardsData from "../components/CardsData"
+import ContactForm from "../components/ContactFrom"
 
+import { IoLogoFirebase } from "react-icons/io5";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaPython } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
 
 export default function TLCLicensing() {
     const cardData = CardsData()
     const bottomColor = {
         primary: '#E2E065',
-        secondary: 'bg-slate-800'
+        secondary: '#1E293B'
+    }
+    const textColor = {
+        colorXL: '#8DB402',
     }
 
     return (
         <>
-            <main className="container mx-auto px-4 flex flex-col gap-20">
+            <main className="container mx-auto px-4 flex flex-col gap-20 md:gap-28">
                 <section className="flex flex-col items-center gap-4 mt-4 pt-4 rounded-t-xl text-slate-800
                     bg-gradient-to-b from-[#E2E065] to-slate-50"
                 >
@@ -40,18 +48,18 @@ export default function TLCLicensing() {
 
                 </section>
 
-                <section>
-                    <div>
-                        <strong>
+                <section className="flex flex-col gap-4 md:w-4/6 m-auto text-slate-800 md:p-8 px-4 py-8 bg-white rounded-2xl">
+                    <div className="md:text-center">
+                        <strong className="text-3xl mb-2">
                             Project Purpose and Goal
                         </strong>
                         <p>
                             This project encompassed three critical phases of site development and integration:
                         </p>
                     </div>
-                    <div>
+                    <div className="flex flex-col gap-4">
                         <div>
-                            <p>
+                            <p className="text-xl" style={{ color: textColor.colorXL }}>
                                 Phase 1: The Biggest Challenge
                             </p>
                             <p>
@@ -61,9 +69,8 @@ export default function TLCLicensing() {
                                 measures that would shield the entire system from unauthorized access.
                             </p>
                         </div>
-
                         <div>
-                            <p>
+                            <p className="text-xl" style={{ color: textColor.colorXL }}>
                                 Phase 2: Ensuring Stable and Secure Connectivity
                             </p>
                             <p>
@@ -72,9 +79,8 @@ export default function TLCLicensing() {
                                 potential breaches.
                             </p>
                         </div>
-
                         <div>
-                            <p>
+                            <p className="text-xl" style={{ color: textColor.colorXL }}>
                                 Phase 3: Advanced Server Port Management
                             </p>
                             <p>
@@ -85,14 +91,18 @@ export default function TLCLicensing() {
                     </div>
                 </section>
 
-                <section>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                <section
+                    className="flex justify-center md:p-4 md:gap-24 gap-4 md:w-4/6 m-auto text-slate-800 rounded-xl"
+                    style={{ backgroundColor: bottomColor.primary }}
+                >
+                    <IoLogoFirebase className="size-16" />
+                    <RiReactjsLine className="size-16" />
+                    <FaPython className="size-16" />
+                    <IoLogoJavascript className="size-16" />
                 </section>
 
-                <section className="container mx-auto px-4">
-                    <strong>
+                <section className="flex flex-col gap-4 md:w-4/6 m-auto text-slate-800">
+                    <strong className="text-3xl">
                         Web Stack and Explanation
                     </strong>
                     <p>
@@ -106,46 +116,105 @@ export default function TLCLicensing() {
                         robust and predictable way to handle complex application logic, enabling efficient data flow and user experience.
                     </p>
 
-                    <div>
-                        <p>
-                            Shopping Cart
-                        </p>
-                        <img src="" alt="" />
-                        <img src="" alt="" />
+                    <div className="flex flex-col my-4 gap-8 justify-center">
+                        <div>
+                            <strong>
+                                Shopping Cart:
+                            </strong>
+                            <img
+                                src="/tlc-images/chopping-cart.png"
+                                alt="TLC Licensing Shopping-Cart page screenshot."
+                                className="rounded-xl my-2 shadow-sm"
+                            />
+                        </div>
+                        <div>
+                            <strong>
+                                Non Exams page:
+                            </strong>
+                            <img
+                                src="/tlc-images/non-exams.png"
+                                alt="TLC Licensing Non-Exams page screenshot."
+                                className="rounded-xl mt-2 shadow-sm"
+                            />
+                        </div>
                     </div>
                 </section>
 
-                <section>
-                    <strong>
+                <section className="flex flex-col gap-4 md:w-4/6 m-auto text-slate-800 md:p-8 px-4 py-8 rounded-2xl
+                    bg-gradient-to-b from-slate-50 to-[#E2E065]"
+                >
+                    <strong
+                        className="text-3xl text-center"
+                        style={{ color: textColor.colorXL }}
+                    >
                         Lessons Learned
                     </strong>
-                    <p>
-                        I could spend hours describing the invaluable lessons learned while working on this project.
-                        The most significant insights came from my deep dive into React Hooks, Git management, Feature Keys,
-                        and API integration. As my first large-scale React project, it became a transformative learning
-                        experience that reshaped my approach to software development.
-                        <br />
-                        Initially, I was prone to writing hasty, unstructured code and quickly moving on. However, this project
-                        taught me a critical lesson: poor code organization can create significant challenges down the line. Now,
-                        I invest substantial time in refactoring and meticulously improving every line of code I write. My focus
-                        has shifted to ensuring maximum readability and maintainability, which ultimately prevents future headaches
-                        and creates more robust, elegant solutions.
-                        <br />
-                        This project was more than just a technical challenge—it was a personal growth journey that fundamentally
-                        changed how I approach software development, emphasizing the importance of clean, thoughtful code and
-                        strategic project management.
-                    </p>
+                    <ol className="flex flex-col gap-4 list-decimal list-inside">
+                        <li>
+                            I could spend hours describing the invaluable lessons learned while working on this project.
+                            The most significant insights came from my deep dive into React Hooks, Git management, Feature Keys,
+                            and API integration. As my first large-scale React project, it became a transformative learning
+                            experience that reshaped my approach to software development.
+                        </li>
+                        <li>
+                            Initially, I was prone to writing hasty, unstructured code and quickly moving on. However, this project
+                            taught me a critical lesson: poor code organization can create significant challenges down the line. Now,
+                            I invest substantial time in refactoring and meticulously improving every line of code I write. My focus
+                            has shifted to ensuring maximum readability and maintainability, which ultimately prevents future headaches
+                            and creates more robust, elegant solutions.
+                        </li>
+                        <li>
+                            This project was more than just a technical challenge—it was a personal growth journey that fundamentally
+                            changed how I approach software development, emphasizing the importance of clean, thoughtful code and
+                            strategic project management.
+                        </li>
+                    </ol>
                 </section>
 
-                <section>
-                    <strong>
+
+
+                <section
+                    id="projects"
+                    className="flex flex-col items-center gap-6 md:gap-12 md:w-5/6 m-auto rounded-2xl"
+                    style={{ backgroundColor: bottomColor.secondary }}
+                >
+                    <strong className="text-4xl mt-4 md:mt-12 text-gradient">
                         Other Projects
                     </strong>
 
-                    {/* Projects card goes here! */}
+                    {cardData.slice(1).map((card, index) => (
+                        <div key={index} className="md:w-4/6">
+                            <a
+                                href={card.link}
+                                target="_blank"
+                                className="card-container"
+                            >
+                                <img src={card.img} alt="Project Image" className="md:card-images rounded-lg" />
+                            </a>
+                            <div>
+                                <div className="flex flex-wrap md:flex-nowrap md:justify-between md:mb-10 px-4 py-4">
+                                    <div className="w-full">
+                                        <a href={card.link} target="_blank">
+                                            <strong className="text-2xl text-slate-200">{card.title}</strong>
+                                        </a>
+                                        <p className="text-slate-400 md:mb-0 mb-2">{card.description}</p>
+                                    </div>
 
+                                    {/* Tech */}
+                                    <div className="flex justify-end gap-2">
+                                        {card.tech.map((technology, idx) => (
+                                            <strong key={idx} className="bg-slate-400 text-slate-800 m-auto px-4 py-2 rounded-md">
+                                                {technology}
+                                            </strong>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </section>
 
+                <ContactForm />
             </main>
         </>
     )
