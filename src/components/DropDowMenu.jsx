5e0da41a87b5eca227408594da382a25104b94df
Daniel Fernandez <mdanif9728@gmail.com>
2024-12-13 11:18:34 -0800

    Add menu-close behavior at the DropDowMenu

--- src/components/DropDowMenu.jsx
+++ src/components/DropDowMenu.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { Menu, Transition, Disclosure, MenuItem, MenuItems } from '@headlessui/react'
+import { Menu, Transition, Disclosure } from '@headlessui/react';
 import CardsData from "./CardsData";
 
 // Icons
@@ -8,9 +8,8 @@ import { RiMenuFoldLine } from "react-icons/ri";
 import { RiMenuFold2Line } from "react-icons/ri";
 import { IoIosArrowDown } from "react-icons/io";
 
-
 export default function DropDowMenu() {
-  const cardsData = CardsData()
+  const cardsData = CardsData();
 
   const scrollToContactSection = (elementId) => {
     const element = document.getElementById(elementId);
@@ -19,19 +18,16 @@ export default function DropDowMenu() {
 
   return (
     <div>
-      <Menu
-        as="div"
-        className="flex content-center relative text-left"
-      >
+      <Menu as="div" className="flex content-center relative text-left">
         <div className="flex self-center">
           <Menu.Button>
-            {({ open }) => (
+            {({ open }) =>
               open ? (
                 <RiMenuFold2Line aria-hidden="true" className="text-red-600 stroke-2 w-6 h-auto md:hidden" />
               ) : (
                 <RiMenuFoldLine aria-hidden="true" className="text-blue-600 stroke-2 w-6 h-auto md:hidden" />
               )
-            )}
+            }
           </Menu.Button>
         </div>
 
@@ -44,82 +40,89 @@ export default function DropDowMenu() {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          {/* ------------ Menu Content ------------ */}
           <Menu.Items className="flex flex-col gap-2 fixed inset-x-0 top-0 top-14 mx-6 mt-8 w-100">
             <div className="flex justify-center px-1 py-4 rounded-xl bg-slate-300 shadow-lg">
-              <MenuItem>
-                <Link
-                  to="/"
-                  className={"flex justify-center gap-1 text-slate-800 w-full px-2 py-2 text-lg"}
-                >
-                  Home
-                </Link>
-              </MenuItem>
-              <MenuItem>
-                <button
-                  onClick={() => scrollToContactSection('contact')}
-                  className={"flex justify-center gap-1 text-slate-800 w-full px-2 py-2 text-lg"}
-                >
-                  Contact
-                </button>
-              </MenuItem>
-              <MenuItem>
-                <Link
-                  to="/about"
-                  className={"flex justify-center gap-1 text-slate-800 w-full px-2 py-2 text-lg"}
-                >
-                  About
-                </Link>
-              </MenuItem>
+              <Menu.Item>
+                {({ close }) => (
+                  <Link
+                    to="/"
+                    onClick={close} 
+                    className={"flex justify-center gap-1 text-slate-800 w-full px-2 py-2 text-lg"}
+                  >
+                    Home
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ close }) => (
+                  <button
+                    onClick={() => {
+                      scrollToContactSection('contact');
+                      close(); 
+                    }}
+                    className={"flex justify-center gap-1 text-slate-800 w-full px-2 py-2 text-lg"}
+                  >
+                    Contact
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ close }) => (
+                  <Link
+                    to="/about"
+                    onClick={close} 
+                    className={"flex justify-center gap-1 text-slate-800 w-full px-2 py-2 text-lg"}
+                  >
+                    About
+                  </Link>
+                )}
+              </Menu.Item>
             </div>
 
-            {/* ------------ Project List ------------ */}
-            <div className='flex flex-col gap-4 py-4 rounded-xl shadow-lg bg-slate-300'>
+            <div className="flex flex-col gap-4 py-4 rounded-xl shadow-lg bg-slate-300">
               <Disclosure defaultOpen>
                 {({ open }) => (
                   <>
                     <Disclosure.Button className="flex px-4 w-full justify-between text-left text-lg text-slate-800">
                       Projects
-                      <span className='flex justify-center gap-2'>
+                      <span className="flex justify-center gap-2">
                         3
                         <IoIosArrowDown
-                          className={`${open ? 'rotate-180 transform' : ''
-                            } h-5 w-5 text-slate-800`}
+                          className={`${
+                            open ? 'rotate-180 transform' : ''
+                          } h-5 w-5 text-slate-800`}
                         />
                       </span>
                     </Disclosure.Button>
 
-                    <Disclosure.Panel className="px-4 pb-2 text-slate-800 ">
+                    <Disclosure.Panel className="px-4 pb-2 text-slate-800">
                       <div className="flex flex-wrap justify-center w-full gap-6 max-h-[55vh] overflow-y-scroll scrollbar-hide">
                         {cardsData.map((card, index) => (
                           <div key={index} className="flex flex-col gap-2">
-                            <Link
-                              to={card.link}
-                            >
-                              <img
-                                src={card.img}
-                                alt="Project Image"
-                                className="object-cover rounded-lg"
-                              />
-                            </Link>
-                            <div className='grid grid-cols-3'>
-                              {/* Col-1 */}
-                              <div className='col-span-2'>
+                            <Menu.Item>
+                              {({ close }) => (
+                                <Link
+                                  to={card.link}
+                                  onClick={close} 
+                                  className="object-cover rounded-lg"
+                                >
+                                  <img src={card.img} alt="Project Image" />
+                                </Link>
+                              )}
+                            </Menu.Item>
+                            <div className="grid grid-cols-3">
+                              <div className="col-span-2">
                                 <strong className="text-xl text-slate-800">
-                                  <Link
-                                    to={card.link}
-                                  >
+                                  <Link to={card.link} target="_blank">
                                     {card.title}
                                   </Link>
                                 </strong>
-                                <p className="text-sm text-slate-800 font-light">
-                                  {card.description}
-                                </p>
+                                <p className="text-sm text-slate-800 font-light">{card.description}</p>
                               </div>
-                              {/* Col-2 */}
-                              <div className='flex p-2 justify-center items-start'>
+                              <div className="flex p-2 justify-center items-start">
                                 <Link
                                   to={card.link}
+                                  target="_blank"
                                   className="w-[80%] p-1 text-slate-800 tracking-wider"
                                 >
                                   Learn More➚
@@ -137,8 +140,6 @@ export default function DropDowMenu() {
           </Menu.Items>
         </Transition>
       </Menu>
-
-
-    </div >
-  )
+    </div>
+  );
 }
