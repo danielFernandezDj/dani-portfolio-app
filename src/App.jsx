da094fa0dc1f3f1d1ee27e5ca8abfb778bfb9f60
Daniel Fernandez <mdanif9728@gmail.com>
2024-12-12 21:50:57 -0800

    fixing the TLC Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 
 // Component
 import DropDowMenu from './components/DropDowMenu';
